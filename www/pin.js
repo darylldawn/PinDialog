6b991aa21c4678a6d079baa0ead173d81ccec10d
darylldawn <daryllvillaos@gmail.com>
2017-03-07 15:39:05 +0900

    added defaultText

--- www/pin.js
+++ www/pin.js
@@ -3,11 +3,12 @@ var exec = require('cordova/exec');
 
 module.exports = {
 
-	prompt: function(message, resultCallback, title, buttonLabels) {
+	prompt: function(message, resultCallback, title, buttonLabels, defaultText) {
         var _message = (message || "Message");
         var _title = (title || "Title");
         var _buttonLabels = (buttonLabels || ["OK","Cancel"]);
-        cordova.exec(resultCallback, null, "PinDialog", "prompt", [_message, _title, _buttonLabels]);
+        var _defaultText = (defaultText || "");
+        cordova.exec(resultCallback, null, "PinDialog", "prompt", [_message, _title, _buttonLabels, _defaultText]);
     }
 
 };
